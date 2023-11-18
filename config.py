5ce9228d944034993dc27a4e4c3bbec5077c9aba
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2023-11-18 13:43:43 +0530

    Removed Environment

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # AZURE AI SEARCH CONFIGURATION
 
-searchservice = "agsearch001"
-index = "agsearchindex001"
-searchkey = "eGb1YvFF0EYMxI7gIIrJRmw4yEIpG3gAWVtLgZ2mzKAzSeA7r2KC"
+searchservice = ""
+index = ""
+searchkey = ""
 category="SEARCH"
 
 #AZURE STORAGE CONFIGURATION
-storageaccount  = "stgagsearch"
-container="docs"
-storagekey="yBeagXzePMQ/ww2TL4rlFsgXsCnRwNflGgwe4urd+xoE5SJ1B2vlj+FO/+QYE1y1fHYfNt7HXj/e+AStF52mnw=="
+storageaccount  = ""
+container=""
+storagekey=""
 
 localpdfparser=True
 verbose=True
 
-FILE_PATH = "C:/Ambarish/AZURE-AI-SEARCH/DOCS/"
+FILE_PATH = ""
 
 formrecognizerservice=""
--- config_openai.py
+++ config_openai.py
@@ -1,4 +1,4 @@
-key = 'd6bcc40e68fc4119abcd43b4661dc8e3'
-location = 'eastus'
-endpoint = 'https://openaidemos007.openai.azure.com/'
-deployment_id_gpt4='gpt4'
+key = ''
+location = ''
+endpoint = ''
+deployment_id_gpt4=''
