0e9adc7e3c4e99df49437d7c7f84c84f3b869dcb
Ambarish Ganguly <ambarish.ganguly@gmail.com>
2023-11-18 13:43:26 +0530

    Removed environment

--- azureaisearch_env/Lib/site-packages/validators/i18n/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Country."""
-
-# isort: skip_file
-
-# local
-from .es import es_cif, es_doi, es_nie, es_nif
-from .fi import fi_business_id, fi_ssn
-
-__all__ = ("fi_business_id", "fi_ssn", "es_cif", "es_doi", "es_nie", "es_nif")
